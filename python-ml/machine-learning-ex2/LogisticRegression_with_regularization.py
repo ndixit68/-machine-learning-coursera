766aae7cf1b4c56781a9bfa99fe6c1b25f09eb1e
Nishant <ndixit68@gmail.com>
2018-04-01 21:39:15 +0800

    calibrated lambda and degree of polynomial expressions

--- python-ml/machine-learning-ex2/LogisticRegression_with_regularization.py
+++ python-ml/machine-learning-ex2/LogisticRegression_with_regularization.py
@@ -23,6 +23,11 @@ data = np.mat(np.genfromtxt('ex2data2.txt', delimiter=','))
 X = data[:, 0:data.shape[1] - 1]
 Y = data[:, data.shape[1] - 1]
 
+# degree of polynomial term - dopt
+dopt = 6
+
+# Set regularization parameter lambda
+lmbd = 0.7
 
 # Plotting Data
 
@@ -36,13 +41,14 @@ def plot_data(X, Y):
     ax.set_xlabel('Exam Score 1')
     ax.set_ylabel('Exam Score 2')
     plt.legend()
-    #plt.show()
+    # plt.show()
     return fig, ax
 
 
 fig, ax = plot_data(X, Y)
 
-#raw_input("Hit enter to continue for Logistic Regression")
+
+# raw_input("Hit enter to continue for Logistic Regression")
 
 
 # ============ Part 2: Compute Cost and Gradient ============
@@ -54,8 +60,10 @@ def sigmoid(X):
 
 def CostFunction(theta, X, Y, lmbd):
     hypothesis = sigmoid(np.dot(X, theta))
-    J = np.mean(-np.multiply(Y.transpose(), np.log(hypothesis)) - np.multiply((1 - Y).transpose(), np.log(1 - hypothesis)))
-    reg_term = lmbd * np.sum(np.square(theta)[1:theta.shape[0]+1])/(2*X.shape[0])  #mind that we don't have to include theta0
+    J = np.mean(
+        -np.multiply(Y.transpose(), np.log(hypothesis)) - np.multiply((1 - Y).transpose(), np.log(1 - hypothesis)))
+    reg_term = lmbd * np.sum(np.square(theta)[1:theta.shape[0] + 1]) / (
+                2 * X.shape[0])  # mind that we don't have to include theta0
     cost = J + reg_term
     return cost
 
@@ -64,10 +72,10 @@ def GradientFunction(theta, X, Y, lmbd):
     hypothesis = sigmoid(np.dot(X, theta))
     error = hypothesis - Y.transpose()
     grad = np.dot(error, X) / Y.size
-    reg_term = lmbd*theta[1:theta.shape[0]+1]/X.shape[0]  #mind that we don't have to include theta0
-    #print reg_term.shape # this should be one less than size of theta
-    gradient = grad + np.append(np.array([0]),reg_term )
-    #print np.append(np.array([0]),reg_term)
+    reg_term = lmbd * theta[1:theta.shape[0] + 1] / X.shape[0]  # mind that we don't have to include theta0
+    # print reg_term.shape # this should be one less than size of theta
+    gradient = grad + np.append(np.array([0]), reg_term)
+    # print np.append(np.array([0]),reg_term)
     return gradient
 
 
@@ -81,7 +89,7 @@ def MapFeature(X1, X2, degree):
     return Mapped_X
 
 
-def PlotDecisonBoundary(theta, X, Y):
+def PlotDecisonBoundary(theta, X, Y, degree=1):
     fig, ax = plot_data(X[:, 1:3], Y)
 
     if X.shape[1] == 3:
@@ -98,8 +106,8 @@ def PlotDecisonBoundary(theta, X, Y):
 
         for i in range(0, u.size):
             for j in range(0, v.size):
-                z[i,j] = np.dot(MapFeature(np.mat(u[i]),np.mat(v[j]),7),theta)
-        ax.contour(u,v,z, np.logspace(-2,-2,1.99999))
+                z[i, j] = np.dot(MapFeature(np.mat(u[i]), np.mat(v[j]), degree), theta)
+        ax.contour(u, v, z, np.logspace(-2, -2, 1.99999))
         plt.show()
 
     return
@@ -107,23 +115,21 @@ def PlotDecisonBoundary(theta, X, Y):
 
 def predict(theta, X):
     m = X.shape[0]
-    p = np.zeros(shape=(m,1))
+    p = np.zeros(shape=(m, 1))
     for iter in range(0, m):
-        probability = float(sigmoid(np.dot(X[iter,:],theta)))
+        probability = float(sigmoid(np.dot(X[iter, :], theta)))
         if probability > 0.5:
             p[iter] = 1
         else:
             p[iter] = 0
     return p
 
+
 # adding polynomial terms in data set
-X = MapFeature(X[:, 0], X[:, 1],7)
+X = MapFeature(X[:, 0], X[:, 1], dopt)
 
 # define initial theta
 initial_theta = np.zeros(X.shape[1])
-
-# Set regularization parameter lambda to 1
-lmbd = 1
 
 # ============= Part 1: Compute and display initial cost and gradient for regularized logistic regression =============
 
@@ -133,7 +139,7 @@ print "Expected Cost (approx) : 0.693 "
 
 grad = GradientFunction(initial_theta, X, Y, lmbd)
 print "Gradient at initial theta (zeros), first five thetas:"
-print grad[0,0:5]
+print grad[0, 0:5]
 
 test_theta = np.ones(X.shape[1])
 
@@ -143,16 +149,15 @@ print "Expected Cost (approx) : 3.16 "
 
 grad = GradientFunction(test_theta, X, Y, 10)
 print "Gradient at initial theta (ones), first five thetas:"
-print grad[0,0:5]
+print grad[0, 0:5]
 
 # ============= Part 2: Optimizing using fmin_tnc =============
 
-lmbd = 1
 theta_opt = opt.fmin_tnc(func=CostFunction, x0=test_theta, fprime=GradientFunction, args=(X, Y, lmbd), messages=0)
-print "Optimized theta", theta_opt[0]
+print "Optimized theta", theta_opt[0][1:5]
 print "Cost with Optimized theta", CostFunction(theta_opt[0], X, Y, lmbd)
 
-PlotDecisonBoundary(theta_opt[0], X, Y);
+PlotDecisonBoundary(theta_opt[0], X, Y, dopt);
 
 # Compute accuracy on our training set
 p = predict(theta_opt[0], X);
