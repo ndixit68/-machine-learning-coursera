12706d4a2bf3b62d5c0c41d74763010f7a3531c2
Nishant <ndixit68@gmail.com>
2018-04-10 06:47:38 +0800

    added the classifiers and tested accuracy in python

--- python-ml/machine-learning-ex3/number_recognition.py
+++ python-ml/machine-learning-ex3/number_recognition.py
@@ -2,6 +2,7 @@ import os.path
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import scipy.io
+import operator
 import scipy.optimize as opt
 from scipy.optimize import fmin_bfgs  # imports the BFGS algorithm to minimize
 import numpy as np
@@ -56,7 +57,7 @@ def display_sample_images(X, sample_size, sample_shape, image_resolution, pad):
 
 
 def check_n_load_dotmat_file():
-    data = scipy.io.loadmat("ex3data1.mat")
+    data = scipy.io.loadmat("/home/nishant/ML-Coursera/programming_exercises/python-ml/machine-learning-ex3/ex3data1.mat")
     returndict = {}
     count = 0
     for i in sorted(data.keys()):
@@ -68,12 +69,12 @@ def check_n_load_dotmat_file():
             print "Creating variable " + i.upper()
             returndict[i.upper()] = np.mat(np.genfromtxt((i.upper() + ".csv"), delimiter=','))
             count = count + 1
-
     variables = [item.strip().upper() for item in sorted(data.keys()) if "__" not in item]
     print len(variables).__str__() + " elements found in the matlab data file, " + str(count) + " loaded"
-
     return returndict
 
+
+#Theta is a an array of dimension (1,), X is a mXn matrix, Y = mX1 matrix lmbd is a float value
 
 def CostFunction(theta, X, Y, lmbd):
     hypothesis = sigmoid(np.dot(X, theta))
@@ -87,6 +88,7 @@ def CostFunction(theta, X, Y, lmbd):
 def sigmoid(X):
     return 1 / (1 + np.exp(- X))
 
+#Theta is a an array of dimension (1,), X is a mXn matrix, Y = mX1 matrix lmbd is a float value
 
 def GradientFunction(theta, X, Y, lmbd):
     hypothesis = sigmoid(np.dot(X, theta))
@@ -98,7 +100,42 @@ def GradientFunction(theta, X, Y, lmbd):
     return gradient
 
 
+def train_one_vs_all_classifier(X, Y, num_labels, lmbd):
+    m = X.shape[0]
+    n = X.shape[1]
+    all_theta = np.zeros(shape=(num_labels, n+1))
+    X = np.append(np.ones(shape=(m,1)),X,1)
+    initial_theta = np.zeros(n+1)
+    for classifier in range(1,num_labels+1): # we're taking range one to ten because in zero is represented as 10 in dataset
+        Y_temp = (Y==classifier).astype(int).transpose()
+        temp = opt.fmin_tnc(func=CostFunction, x0=initial_theta, fprime=GradientFunction, args=(X,Y_temp,lmbd), messages=0)
+        if classifier == num_labels:
+            all_theta[0, :] = temp[0]
+        else:
+            all_theta[classifier, :] = temp[0]
+    return all_theta
+
+
+def predict_one_vs_all(X, all_theta):
+    m = X.shape[0]
+    n = X.shape[1]
+    X = np.append(np.ones(shape=(m, 1)), X, 1)
+    p = np.zeros(shape=(m,1))
+    for ex_set in range(0, m):
+        predicted_values = sigmoid(np.dot(X[ex_set,:],all_theta.transpose()))
+        index_of_max = np.argmax(predicted_values)
+        max_value = predicted_values[0,index_of_max]
+        # print index_of_max, max_value
+        if max_value >= 0.5:
+            if index_of_max == 0:
+                p[ex_set,:] = 10 #because in zero is represented as 10
+            else:
+                p[ex_set,:] = index_of_max
+    return p
+
+
 if __name__ == "__main__":
+
     # check if the variables in the matlab file are not converted to csv , if not load data in matlab file
     data = check_n_load_dotmat_file()
 
@@ -124,8 +161,20 @@ if __name__ == "__main__":
 
     print 'Cost: ', J
     print 'Expected cost: 2.534819\n'
-    print 'Gradients: ', grad
+    print 'Gradients: ', grad, grad.shape
     print 'Expected gradients:\n'
     print ' 0.146561\n -0.548558\n 0.724722\n 1.398003\n'
 
-    print 'Program paused. Press enter to continue.\n'
+    raw_input('Program paused. Press enter to continue to train a classifier')
+
+    # train one vs all classifier using logistic regression
+    num_labels = 10
+    lmbd = 0.00001
+
+    all_theta = train_one_vs_all_classifier(X, Y, num_labels, lmbd)
+    #print all_theta.shape
+
+    #predict the values usingpredictedvalue trained thetas to calculate accuracy
+    p = predict_one_vs_all(X, all_theta)
+    print "Training set accuracy" , np.mean(np.double(p == np.transpose(Y)))*100
+
