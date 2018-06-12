676a4730ad832ef45c5bbfed2338b4f73621c05a
Nishant <ndixit68@gmail.com>
2018-06-12 09:41:09 +0800

    Updating Python solution

--- python-ml/machine-learning-ex4/FirstNeuralNetwork.py
+++ python-ml/machine-learning-ex4/FirstNeuralNetwork.py
@@ -74,14 +74,43 @@ def check_n_load_dotmat_file(filename):
 
 # Theta is a an array of dimension (1,), X is a mXn matrix, Y = mX1 matrix lmbd is a float value
 
-def CostFunction(theta, X, Y, lmbd):
-    hypothesis = sigmoid(np.dot(X, theta))
-    J = np.mean(
-        -np.multiply(Y.transpose(), np.log(hypothesis)) - np.multiply((1 - Y).transpose(), np.log(1 - hypothesis)))
-    reg_term = lmbd * np.sum(np.square(theta)[1:theta.shape[0] + 1]) / (2.0 * X.shape[0])
+def nnCostFunction(nn_params, input_layer_size, hidden_layer_size, num_labels, X, Y, lmbd):
+
+    # unroll thetas
+
+    Theta1 = np.reshape(nn_params[0:hidden_layer_size * (input_layer_size + 1)], newshape=(hidden_layer_size, (input_layer_size + 1)), order='F')
+    Theta2 = np.reshape(nn_params[(hidden_layer_size * (input_layer_size + 1)):], newshape=(num_labels, (hidden_layer_size + 1)), order='F')
+
+    m = X.shape[0]
+    Theta1_grad = np.zeros(shape=(Theta1.shape))
+    Theta2_grad = np.zeros(shape=(Theta2.shape))
+
+    # for input Layer l=1
+    A1 = np.append(np.ones(shape=(m,1)),X, 1)
+
+
+    # for hidden layer l=2
+    Z2 = np.dot(A1, Theta1.transpose())
+    A2 = sigmoid(Z2)
+    A2 = np.append(np.ones(shape=(m,1)),A2, 1)
+
+    # for output layer l=3
+    Z3 = np.dot(A2, Theta2.transpose())
+    h_theta = sigmoid(Z3)
+
+    # convert y into m x K array
+    new_y = np.zeros(shape=(m, num_labels))
+    for i in range(0, Y.shape[0]):
+        new_y[i, int(Y[i]%num_labels)] = 1.0
+
+    #cost =
+
+    J = np.sum(np.mean(-np.multiply(new_y.transpose(), np.log(h_theta)) - np.multiply((1 - new_y).transpose(), np.log(1 - h_theta))))
+
+    #reg_term = lmbd * np.sum(np.square(theta)[1:theta.shape[0] + 1]) / (2.0 * X.shape[0])
     # mind that we don't have to include theta0
-    cost = J + reg_term
-    return cost
+    #cost = J + reg_term
+    return J
 
 
 def sigmoid(X):
@@ -101,11 +130,16 @@ def GradientFunction(theta, X, Y, lmbd):
 
 
 if __name__ == "__main__":
+
+    #Defining size of neural network
+    input_layer_size = 400   # 20x20 Input Images of Digits
+    hidden_layer_size = 25   # 25 hidden units
+    num_labels = 10          # 10 labels, from 1 to 10   (note that we have mapped "0" to label 10)
+
     # check if the variables in the matlab file are not converted to csv , if not load data in matlab file
     data = check_n_load_dotmat_file("ex4data1.mat")
 
     # check some images from the dataset loaded
-
     X = data['X']
     Y = data['Y']
 
@@ -114,8 +148,28 @@ if __name__ == "__main__":
 
     display_sample_images(X, 100, [10, 10], [20, 20], 1)
 
-    nn_params =
+    raw_input("Hit enter to continue and load parameters")
 
-    raw_input("Hit enter to continue and test the logistic regression")
+    print('\nLoading parameters.....')
 
-    print('\nTesting CostFunction with regularization')
+    params = check_n_load_dotmat_file("ex4weights.mat")
+
+    Theta1 = np.array(params['THETA1'])
+    Theta2 = np.array(params['THETA2'])
+
+    print "Theta1 shape: " + str(Theta1.shape)
+    print "Theta2 shape: " + str(Theta2.shape)
+    print "Theta1 shape: " + str(Theta1.flatten('F').shape)
+    print "Theta2 shape: " + str(Theta2.flatten('F').shape)
+
+    nn_params = np.append(Theta1.flatten('F'), Theta2.flatten('F'), axis=0)
+
+    raw_input("Hit enter to feed forward using Neural Network")
+
+    lmbd = 0
+
+    J = nnCostFunction(nn_params, input_layer_size, hidden_layer_size, num_labels, X, Y, lmbd)
+    print J
+
+
+
